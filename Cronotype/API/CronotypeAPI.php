5d74b101bcaca4384deb1c009af362cd362608d9
xavifm <60983549+xavifm@users.noreply.github.com>
2023-03-01 13:46:38 +0100

    modified the api to be stored in json

--- Cronotype/API/CronotypeAPI.php
+++ Cronotype/API/CronotypeAPI.php
@@ -1,7 +1,5 @@
 <?php 
     ini_set('display_errors', 0);
-    include 'API_Questions.php';
-    include 'API_Responses.php';
     include 'API_TestResult.php';
 
     if($_POST["GetTestResult"] == "true") 
@@ -67,90 +65,44 @@
 
         public function GetQuestionsArray() 
         {
-            $questionsData = new QuestionsData();
 
-            $questionsArray = array(
-                $questionsData->gender,
-                $questionsData->age,
-                $questionsData->soundsWakeMeUp,
-                $questionsData->snoreWhileSleeping,
-                $questionsData->snoreWhileSleeping_often,
-                $questionsData->snoringType,
-                $questionsData->foodPassion,
-                $questionsData->wakeUpBeforeMyAlarmRings,
-                $questionsData->sleepOnPlanes,
-                $questionsData->irritableDueFatigue,
-                $questionsData->worriesInSmallDetails,
-                $questionsData->insomniacDiagnosis,
-                $questionsData->anxiousAboutMyGrades,
-                $questionsData->SleepRumiation,
-                $questionsData->perfectionist,
-                $questionsData->wakeUpHourFreeMode,
-                $questionsData->alarmClock,
-                $questionsData->wakeUpOnWeekends,
-                $questionsData->jetLag,
-                $questionsData->favoriteMeal,
-                $questionsData->concentration,
-                $questionsData->workout,
-                $questionsData->mostAlert,
-                $questionsData->fiveHourWorkday,
-                $questionsData->brainQuestion,
-                $questionsData->nap,
-                $questionsData->efficiencyAndSaferHours,
-                $questionsData->betterStatement,
-                $questionsData->riskTakingComfort,
-                $questionsData->selfConsider,
-                $questionsData->studentType,
-                $questionsData->firstWakeUp,
-                $questionsData->appetiteInMorning,
-                $questionsData->insomniaIntensity,
-                $questionsData->lifeSatisfaction
-            );
+            $jsonFile = file_get_contents('Questionary.json', true);
+            $jsonResult = json_decode($jsonFile);
+
+            $questionsArray = array();
+
+            foreach ($jsonResult as &$firstArray) 
+            {
+                foreach($firstArray as &$question) 
+                {
+                    foreach($question as &$content) 
+                    {
+                        array_push($questionsArray, $content->title);
+                    }
+                }
+            }
 
             return $questionsArray;
         }
 
         public function GetResponsesArray() 
         {
-            $questionsData = new QuestionsData();
+            $jsonFile = file_get_contents('Questionary.json', true);
+            $jsonResult = json_decode($jsonFile);
 
-            $responsesDataArray = array(
-                $questionsData->gender_R,
-                $questionsData->age_R,
-                $questionsData->soundsWakeMeUp_R,
-                $questionsData->snoreWhileSleeping_R,
-                $questionsData->snoreWhileSleeping_often_R,
-                $questionsData->snoringType_R,
-                $questionsData->foodPassion_R,
-                $questionsData->wakeUpBeforeMyAlarmRings_R,
-                $questionsData->sleepOnPlanes_R,
-                $questionsData->irritableDueFatigue_R,
-                $questionsData->worriesInSmallDetails_R,
-                $questionsData->insomniacDiagnosis_R,
-                $questionsData->anxiousAboutMyGrades_R,
-                $questionsData->SleepRumiation_R,
-                $questionsData->perfectionist_R,
-                $questionsData->wakeUpHourFreeMode_R,
-                $questionsData->alarmClock_R,
-                $questionsData->wakeUpOnWeekends_R,
-                $questionsData->jetLag_R,
-                $questionsData->favoriteMeal_R,
-                $questionsData->concentration_R,
-                $questionsData->workout_R,
-                $questionsData->mostAlert_R,
-                $questionsData->fiveHourWorkday_R,
-                $questionsData->brainQuestion_R,
-                $questionsData->nap_R,
-                $questionsData->efficiencyAndSaferHours_R,
-                $questionsData->betterStatement_R,
-                $questionsData->riskTakingComfort_R,
-                $questionsData->selfConsider_R,
-                $questionsData->studentType_R,
-                $questionsData->firstWakeUp_R,
-                $questionsData->appetiteInMorning_R,
-                $questionsData->insomniaIntensity_R,
-                $questionsData->lifeSatisfaction_R
-            );
+            $responsesDataArray = array();
+
+            foreach ($jsonResult as &$firstArray) 
+            {
+                foreach($firstArray as &$question) 
+                {
+                    foreach($question as &$content) 
+                    {
+                        array_push($responsesDataArray, $content->responses);
+                    }
+                }
+            }
+
 
             return $responsesDataArray;
         }
